a59b335e057d65eb5c064fd38a51f828afe24b0d
Jose Rivera <jpr47@njit.edu>
2023-12-11 16:22:09 -0500

    added springboot project

---
+++ PetPlanner/src/main/java/com/cognixia/jump/pets/PetPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.cognixia.jump.pets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetPlannerApplication.class, args);
+	}
+
+}
