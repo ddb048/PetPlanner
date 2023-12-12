1107ce2f0bfc9bba9cdb22557edab1f9b4a6637c
ddb048 <106298312+ddb048@users.noreply.github.com>
2023-12-12 09:05:48 -0800

    user created

---
+++ PetPlanner/src/main/java/com/cognixia/jump/model/User.java
@@ -0,0 +1,32 @@
+// package com.cognixia.jump.model;
+
+// import java.io.Serializable;
+
+// import javax.persistence.Entity;
+// import javax.persistence.EnumType;
+// import javax.persistence.Enumerated;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.GenerationType;
+// import javax.persistence.Id;
+// import javax.persistence.JoinColumn;
+// import javax.persistence.ManyToOne;
+// import javax.validation.constraints.Digits;
+
+// @Entity
+// @Table(name = "users")
+// public class User {
+
+// @GeneratedValue(strategy = GenerationType.IDENTITY)
+// private Long id;
+
+// @Column(unique = true)
+// private String username;
+
+// private String password;
+
+// @Column(unique = true)
+// private String email;
+
+// private String profilePicture;
+
+// }
