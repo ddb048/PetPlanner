3776ccba745835916327a85d943b708d445361e1
Jose Rivera <jpr47@njit.edu>
2023-12-12 12:53:49 -0500

    added pets model class

---
+++ PetPlanner/src/main/java/com/cognixia/jump/model/Pet.java
@@ -0,0 +1,150 @@
+package com.cognixia.jump.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "pets")
+public class Pet implements Serializable {
+	
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="ownerId")
+	private User ownerId;
+	
+	@NotBlank
+	@Enumerated(EnumType.STRING)
+	private Species species;
+	
+	private String petPicture;
+	
+	@Temporal(TemporalType.DATE)
+	private Date birthdate;
+	
+	@NotBlank
+	@Enumerated(EnumType.STRING)
+	private Temperament temparement;
+	
+	@Column(length=500)
+	private String description;
+	
+	@ManyToMany
+    @JoinTable(
+        name = "petsEvents",
+        joinColumns = @JoinColumn(name = "petId"),
+        inverseJoinColumns = @JoinColumn(name = "eventId")
+    )
+    private Set<Event> events = new HashSet<>();
+
+	public Pet()
+	{
+		
+	}
+	
+	public Pet(Long id, User ownerId, Species species, String petPicture, Date birthdate, Temperament temparement,
+			String description) {
+		super();
+		this.id = id;
+		this.ownerId = ownerId;
+		this.species = species;
+		this.petPicture = petPicture;
+		this.birthdate = birthdate;
+		this.temparement = temparement;
+		this.description = description;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getOwnerId() {
+		return ownerId;
+	}
+
+	public void setOwnerId(User ownerId) {
+		this.ownerId = ownerId;
+	}
+
+	public Species getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(Species species) {
+		this.species = species;
+	}
+
+	public String getPetPicture() {
+		return petPicture;
+	}
+
+	public void setPetPicture(String petPicture) {
+		this.petPicture = petPicture;
+	}
+
+	public Date getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public Temperament getTemparement() {
+		return temparement;
+	}
+
+	public void setTemparement(Temperament temparement) {
+		this.temparement = temparement;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	public enum Species {
+        DOG, CAT, BIRD, FISH, REPTILE, OTHER
+    }
+
+    public enum Temperament {
+        FRIENDLY, RESERVED, AGGRESSIVE, PLAYFUL, CALM
+    }
+	
+   //TO-DO:  ToString method
+
+}
--- PetPlanner/src/main/java/com/cognixia/jump/model/User.java
+++ PetPlanner/src/main/java/com/cognixia/jump/model/User.java
@@ -1,11 +1,16 @@
 package com.cognixia.jump.model;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -39,6 +44,27 @@ public class User implements Serializable {
 
 	
 	private String profilePic;
+	
+	@OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
+	private Set<Pet> pets = new HashSet<>();
+	
+	
+	@OneToMany(mappedBy = "organizer", cascade = CascadeType.ALL, orphanRemoval = true)
+	private Set<Event> organizedEvents = new HashSet<>();
+	
+	
+	public User(Long id,
+			@NotBlank(message = "Username is required") @Size(min = 4, max = 20, message = "Username must be between 4 and 20 characters") String username,
+			@NotBlank(message = "Password is required") @Size(min = 6, message = "Password must have at least 6 characters") String password,
+			@NotBlank(message = "Email is required") @Email(message = "Email should be valid") String email,
+			String profilePic) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.profilePic = profilePic;
+	}
 	
 	
 	public User()
@@ -84,16 +110,31 @@ public class User implements Serializable {
 	public void setProfilePic(String profilePic) {
 		this.profilePic = profilePic;
 	}
+	
+	public Set<Pet> getPets() {
+        return pets;
+    }
 
-	@Override
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public Set<Event> getOrganizedEvents() {
+        return organizedEvents;
+    }
+
+    public void setOrganizedEvents(Set<Event> organizedEvents) {
+        this.organizedEvents = organizedEvents;
+    }
+
+
+	//TO-DO: include events and pets in To String method
+    @Override
 	public String toString() {
-		return "User [id=" + id + ", userName=" + username + ", password=" + password + ", email=" + email
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", email=" + email
 				+ ", profilePic=" + profilePic + "]";
 	}
-	
-	
-	
-	
-	
 
+	
+	
 }
