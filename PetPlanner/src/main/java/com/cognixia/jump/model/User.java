7440ba0cb51a83bdf06bf471a3e9d0e7810ac420
Jose Rivera <jpr47@njit.edu>
2023-12-12 12:19:09 -0500

    added validation annotation

--- PetPlanner/src/main/java/com/cognixia/jump/model/User.java
+++ PetPlanner/src/main/java/com/cognixia/jump/model/User.java
@@ -2,20 +2,58 @@
 //
 //import java.io.Serializable;
 //
-//import jakarta.persistence.Entity;
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//
+////import javax.persistence.*;
+////import javax.validation.constraints.Email;
+////import javax.validation.constraints.NotBlank;
+////import javax.validation.constraints.Size;
+////
+////@Entity
+////@Table(name = "users")
+////public class User {
+////
+////    @Id
+////    @GeneratedValue(strategy = GenerationType.IDENTITY)
+////    private Long id;
+////
+////    @NotBlank(message = "Username is required")
+////    @Size(min = 4, max = 20, message = "Username must be between 4 and 20 characters")
+////    @Column(unique = true)
+////    private String username;
+////
+////    @NotBlank(message = "Password is required")
+////    @Size(min = 6, message = "Password must have at least 6 characters")
+////    private String password;
+////
+////    @NotBlank(message = "Email is required")
+////    @Email(message = "Email should be valid")
+////    @Column(unique = true)
+////    private String email;
+//
+//
 //
 //@Entity
 //public class User implements Serializable {
 //	
 //	private static final long serialVersionUID = 1L;
 //	
-//	
+//	@Id
+//	@GeneratedValue(strategy=GenerationType.IDENTITY)
 //	private Integer id;
-//
+//	
+//	@Column(unique=true, nullable=false)
 //	private String userName;
 //	
+//	@Column(nullable=false)
 //	private String password;
 //	
+//	
+//	@Column(nullable=false)
 //	private String email;
 //	
 //	private String profilePic;
