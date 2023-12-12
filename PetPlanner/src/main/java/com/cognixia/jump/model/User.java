d91cc504f92fbada2b9fb84843e81a2cc631edf3
Jose Rivera <jpr47@njit.edu>
2023-12-12 12:03:05 -0500

    updated version

---
+++ PetPlanner/src/main/java/com/cognixia/jump/model/User.java
@@ -0,0 +1,79 @@
+//package com.cognixia.jump.model;
+//
+//import java.io.Serializable;
+//
+//import jakarta.persistence.Entity;
+//
+//@Entity
+//public class User implements Serializable {
+//	
+//	private static final long serialVersionUID = 1L;
+//	
+//	
+//	private Integer id;
+//
+//	private String userName;
+//	
+//	private String password;
+//	
+//	private String email;
+//	
+//	private String profilePic;
+//	
+//	
+//	public User()
+//	{
+//		
+//	}
+//	public Integer getId() {
+//		return id;
+//	}
+//
+//	public void setId(Integer id) {
+//		this.id = id;
+//	}
+//
+//	public String getUserName() {
+//		return userName;
+//	}
+//
+//	public void setUserName(String userName) {
+//		this.userName = userName;
+//	}
+//
+//	public String getPassword() {
+//		return password;
+//	}
+//
+//	public void setPassword(String password) {
+//		this.password = password;
+//	}
+//
+//	public String getEmail() {
+//		return email;
+//	}
+//
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//
+//	public String getProfilePic() {
+//		return profilePic;
+//	}
+//
+//	public void setProfilePic(String profilePic) {
+//		this.profilePic = profilePic;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "User [id=" + id + ", userName=" + userName + ", password=" + password + ", email=" + email
+//				+ ", profilePic=" + profilePic + "]";
+//	}
+//	
+//	
+//	
+//	
+//	
+//
+//}
