cc81ba4311b40ed8c2c6968c9bb2633259cdf650
Jose Rivera <jpr47@njit.edu>
2023-12-12 16:11:15 -0500

    added some secutity files

---
+++ PetPlanner/src/main/java/com/cognixia/jump/service/MyUserDetails.java
@@ -0,0 +1,87 @@
+package com.cognixia.jump.service;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.cognixia.jump.model.Customer;
+
+// UserDetails class -> inherit from the UserDetails interface
+//					 -> used by spring security to find all the necessary info for authorization & authentication
+public class MyUserDetails implements UserDetails {
+	
+	private static final long serialVersionUID = 1L;
+
+	
+	private String username;
+	private String password;
+	private boolean enabled;
+	private List<GrantedAuthority> authorities;
+	
+	
+	public MyUserDetails(Customer customer) {
+		
+		this.username = customer.getUsername();
+		this.password = customer.getPassword();
+		this.enabled = customer.isEnabled();
+		
+		// Granted Authorities -> permissions as a user (which endpoints can we access)
+		//					   -> can find our GA based on what the user's roles are
+		this.authorities = Arrays.asList( new SimpleGrantedAuthority( customer.getRole().name() ) );
+	}
+	
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		return username;
+	}
+	
+	
+	/*
+	 	All Methods After Here:
+	 		- DON'T NEED to store this type of info in the user table
+	 		- store this info if its worthwhile for your security
+	 		- have these methods return true manually if you don't need to store info about them
+	 		  for your security
+	*/
+	
+	
+	// accounts can expire b/c temporary accounts
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	// lock an account b/c a person guessed the wrong pw too many times
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	// credentials are no longer valid b/c you have to set a new pw every couple months
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	// enabled -> can't/can use this account
+	@Override
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+}
---
+++ PetPlanner/src/main/java/com/cognixia/jump/service/MyUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.cognixia.jump.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	UserRepository repo;
+
+	
+	// method will by called by Spring Security when a request comes in
+	// credentials (username + password) passed through the request will be loaded in
+	// username will be passed to this method (as an argument), then will call the UserRepository in order to find a user with that username
+	// As long as this user is found, User info will be passed to a UserDetails object and returned
+		
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		Optional<Customer> customerFound = repo.findByUsername(username);
+		
+		// if username doesn't exist in the table, throw an exception
+		if(customerFound.isEmpty()) {
+			throw new UsernameNotFoundException("No user with username = " + username);
+		}
+		
+		// as long as we found the user, create a user details object with all the relevant info for security 
+		// security will take this object and perform authorization & authentication
+		return new MyUserDetails( customerFound.get() );
+	}
+
+}
