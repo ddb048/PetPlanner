a0a6d098518cce0dd2546eb950a2961c0e8ba570
ddb048 <106298312+ddb048@users.noreply.github.com>
2023-12-14 11:42:37 -0800

    removed tests

--- PetPlanner/src/main/java/com/cognixia/jump/config/SecurityConfiguration.java
+++ PetPlanner/src/main/java/com/cognixia/jump/config/SecurityConfiguration.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -34,12 +36,14 @@ public class SecurityConfiguration {
 	JwtRequestFilter jwtRequestFilter;
 
 	@Bean
+	@Order(Ordered.HIGHEST_PRECEDENCE)
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(Arrays.asList("*"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept"));
 		configuration.setAllowCredentials(true);
+		configuration.addExposedHeader("Access-Control-Allow-Origin");
 
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
